8aeac3765b593f35798f0be7b57fe62cd8d08796
Advaith <hereliesadvaith@gmail.com>
2023-02-25 16:00:35 +0530

    reactDjango

---
+++ react_django/boilerplate-pro/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+const reactlogopath = "/static/react.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Django React Development</h1>
+      <img src={reactlogopath} alt="react" />
+    </div>
+  )
+}
+
+export default App
